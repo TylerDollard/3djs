8097b45f58e7bf99504b0c677a3c5111fea133da
TylerDollard <124917541+TylerDollard@users.noreply.github.com>
2024-11-06 15:41:22 -0500

    Updates

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import * as THREE from 'three';
+// Setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+// create a new renderer by instating the canvas element in our HTML // file
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+});
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
